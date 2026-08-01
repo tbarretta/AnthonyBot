b4d3e8a63ca15130e9fcc6d60823600d2ed3e2fc
tbarretta <tbarretta@users.noreply.github.com>
2026-07-31 23:21:02 -0400

    Fix 500 error on Comments admin page caused by invalid list_filter

--- projects/no-double-u/apps/discussion/admin.py
+++ projects/no-double-u/apps/discussion/admin.py
@@ -1,10 +1,26 @@
 from django.contrib import admin
 from .models import Comment
 
+class IsTopicFilter(admin.SimpleListFilter):
+    title = 'is topic'
+    parameter_name = 'is_topic'
+
+    def lookups(self, request, model_admin):
+        return (
+            ('Yes', 'Yes'),
+            ('No', 'No'),
+        )
+
+    def queryset(self, request, queryset):
+        if self.value() == 'Yes':
+            return queryset.filter(parent__isnull=True)
+        if self.value() == 'No':
+            return queryset.filter(parent__isnull=False)
+
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
     list_display = ('author_name', 'is_topic', 'created_at', 'is_approved', 'is_flagged')
-    list_filter = ('is_approved', 'is_flagged', 'parent__isnull', 'created_at')
+    list_filter = ('is_approved', 'is_flagged', IsTopicFilter, 'created_at')
     search_fields = ('author_name', 'body')
     actions = ['approve_comments', 'unapprove_comments', 'unflag_comments']
     filter_horizontal = ('flagged_by',)
